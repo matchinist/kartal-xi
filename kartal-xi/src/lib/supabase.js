76ec429527a3c830e94863dea6b10b055d786a78
matchinist <41239823+matchinist@users.noreply.github.com>
2026-07-03 14:02:43 +0200

    Fix Supabase anon key

--- kartal-xi/src/lib/supabase.js
+++ kartal-xi/src/lib/supabase.js
@@ -1,4 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
+
 const supabaseUrl = 'https://fojysisgshhhcmsnujrh.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvanlzaXNnc2hoaGNtc251anJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk5MDgwNzMsImV4cCI6MjAyNTQ4NDA3M30.GUd2TqUaMkEdYRcJnOTkfLSYP7Lw7yAZqMjsI0s35Cc';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZvanlzaXNnc2hoaGNtc251anJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5MzE3MTksImV4cCI6MjA5NjUwNzcxOX0.CcBe4kVbS3aatbXpHZ_-TCbMzMu2e5xU5RavW6nauWk';
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
